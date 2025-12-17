634f75424dabf79a372d62637bae0c5af5873fe2
Kievsky332 <falileevry@gmail.com>
2025-12-17 20:37:32 +0300

    Эхо бот +кнопки

--- index.py
+++ index.py
@@ -1,11 +1,69 @@
 import telebot
 import secret
+import random
+import hashlib
+from os import system
 
 bot = telebot.TeleBot(secret.bot)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
-#Функция повтора сообщений
+print("Бот запущен")
+#Рандомный код 
+@bot.message_handler(commands=["code"] )
+@bot.message_handler(regexp="Получить код" )
+def repeat_all_messages(message): 
+    b = str(random.randint(100,100000))
+    ba = secret.a+b+secret.b
+    c = hashlib.md5(ba.encode()).hexdigest()
+    bot.send_message(message.chat.id, c)
 
+
+#Убийство бота
+@bot.message_handler(commands=[secret.kill] )
+def kill(message):
+    bot.send_message(message.chat.id, "Пошел нахуй мразь ,ладно иду спать!")
+    bot.stop_polling()
+    system("clear||cls")
+    print("Бот убит через комманду")
+
+#Поддержка
+@bot.message_handler(commands=["help"])
+@bot.message_handler(regexp="Связь с поддержкой" )
+def hiAi(message): 
+    bot.send_message(message.chat.id, "Связь с поддержкой")
+
+#Донат
+@bot.message_handler(regexp="Донат" )
+def hiAi(message): 
+    bot.send_message(message.chat.id, "Донат")
+
+#Отзывы
+@bot.message_handler(regexp="Отзывы" )
+def hiAi(message): 
+    bot.send_message(message.chat.id, "Отзывы")
+
+#Оставить отзыв
+@bot.message_handler(regexp="Оставить отзыв" )
+def hiAi(message): 
+    bot.send_message(message.chat.id, "отставьте отзыв")
+
+
+#Кнопки
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True , row_width=2)
+    kod = telebot.types.KeyboardButton(text="Получить код" )
+    help = telebot.types.KeyboardButton(text="Связь с поддержкой" )
+
+    sendotz = telebot.types.KeyboardButton(text="Оставить отзыв")
+    otz = telebot.types.KeyboardButton(text="Отзывы")
+
+    dnt = telebot.types.KeyboardButton(text="Донат")
+    keyboard.add(kod, help,sendotz,otz,dnt)
+    bot.send_message(chat_id,
+                     'Привет это оффициальный бот h vpn - howdy vpn. Здесь ты можешь получить код. \n Мы принимаем в оплату в крипте ,и звездами ,сбп.',
+                     reply_markup=keyboard)
+    
+
+    #Нон стоп бот!
 bot.polling(none_stop=True)
